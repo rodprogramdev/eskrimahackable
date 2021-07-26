f121d39d2ffce4c07041e71a3d9bfbb4008ae8f4
Rodrigo Lopez <rodprogramdev@gmail.com>
2021-07-26 15:02:18 +0800

    restored game5

--- game5/carRaceJs/Car.js
+++ game5/carRaceJs/Car.js
@@ -12,20 +12,36 @@ const MIN_SPEED_TO_TURN = 0.5;
 
 /******************************CLASS TO CREATE INSTANCES******************************************/
 
-function carClass() {
-  this.x = 75; //We will use this value in our arcMethod x axis
-  this.y = 75;
-  this.ang = 0;
-  this.speed = 0;
+// function carClass() {
+//   this.x = 75; //We will use this value in our arcMethod x axis
+//   this.y = 75;
+//   this.ang = 0;
+//   this.speed = 0;
 
-  // var carX = 75; //We will use this value in our arcMethod x axis
-  // var carY = 75;
-  // var carAng = 0;
-  // var carSpeed = 0;
+  var carX = 75; //We will use this value in our arcMethod x axis
+  var carY = 75;
+  var carAng = 0;
+  var carSpeed = 0;
 
   /****************************FUNCTION CAR RESET*************************************************/
 
-  // function carReset() {
+   function carReset() {
+     for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+       for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
+         var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+         if (trackGrid[arrayIndex] == 2) {
+           trackGrid[arrayIndex] = 0;
+           // carAng = -90 * Math.PI/180.0; //Multiply * radiance math.pi
+           carAng = -Math.PI / 2;
+           carX = eachCol * TRACK_W;
+           carY = eachRow * TRACK_H;
+         }
+       }
+     }
+   }
+
+  
+  // this.reset() = function() {
   //   for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
   //     for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
   //       var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
@@ -33,42 +49,46 @@ function carClass() {
   //         trackGrid[arrayIndex] = 0;
   //         // carAng = -90 * Math.PI/180.0; //Multiply * radiance math.pi
   //         carAng = -Math.PI / 2;
-  //         carX = eachCol * TRACK_W;
-  //         carY = eachRow * TRACK_H;
-  //       }
-  //     }
-  //   }
-  // }
-
-  
-  this.reset() = function() {
-    for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
-      for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
-        var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
-        if (trackGrid[arrayIndex] == 2) {
-          trackGrid[arrayIndex] = 0;
-          // carAng = -90 * Math.PI/180.0; //Multiply * radiance math.pi
-          carAng = -Math.PI / 2;
-          this.x = eachCol * TRACK_W;
-          this.y= eachRow * TRACK_H;
-        } //end of player start if
-      } // end of col for
-    }// endo frow for
-  }// end of carReset function
+  //         this.x = eachCol * TRACK_W;
+  //         this.y= eachRow * TRACK_H;
+  //       } //end of player start if
+  //     } // end of col for
+  //   }// endo frow for
+  // }// end of carReset function
 
   /****************************FUNCTION CAR MOVE*************************************************/
   //Lets look at the code in the moveAll function on when does the car hit the bottom of the screen.
-  // function carMove() {
-  //   carSpeed *= GROUNDSPEED_DECAY_MULT;
+  function carMove() {
+     carSpeed *= GROUNDSPEED_DECAY_MULT;
+     if (keyHeld_Gas) {
+       carSpeed += DRIVE_POWER;
+     }
+     if (keyHeld_Reverse) {
+       carSpeed -= REVERSE_POWER;
+     }
+     if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
+       if (keyHeld_TurnLeft) {
+         carAng -= TURN_RATE;
+       }
+       if (keyHeld_TurnRight) {
+         carAng += TURN_RATE;
+       }
+     }
+     carX += Math.cos(carAng) * carSpeed;
+     carY += Math.sin(carAng) * carSpeed;
+   //carAng += 0.02; //Makes the car spin
+   }
+  // this.move= function() {
+  //   this.speed *= GROUNDSPEED_DECAY_MULT;
   //   if (keyHeld_Gas) {
-  //     carSpeed += DRIVE_POWER;
+  //     this.speed += DRIVE_POWER;
   //   }
   //   if (keyHeld_Reverse) {
-  //     carSpeed -= REVERSE_POWER;
+  //     this.speed -= REVERSE_POWER;
   //   }
   //   if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
   //     if (keyHeld_TurnLeft) {
-  //       carAng -= TURN_RATE;
+  //       this.ang -= TURN_RATE;
   //     }
   //     if (keyHeld_TurnRight) {
   //       carAng += TURN_RATE;
@@ -78,37 +98,17 @@ function carClass() {
   //   carY += Math.sin(carAng) * carSpeed;
   //   // carAng += 0.02; Makes the car spin
   // }
-  this.move= function() {
-    this.speed *= GROUNDSPEED_DECAY_MULT;
-    if (keyHeld_Gas) {
-      this.speed += DRIVE_POWER;
-    }
-    if (keyHeld_Reverse) {
-      this.speed -= REVERSE_POWER;
-    }
-    if (Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
-      if (keyHeld_TurnLeft) {
-        this.ang -= TURN_RATE;
-      }
-      if (keyHeld_TurnRight) {
-        carAng += TURN_RATE;
-      }
-    }
-    carX += Math.cos(carAng) * carSpeed;
-    carY += Math.sin(carAng) * carSpeed;
-    // carAng += 0.02; Makes the car spin
-  }
 
-  // function carDraw() {
+ function carDraw() {
+   drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
+    if(carPicLoaded){
+     drawBitmapCenteredWithRotation(carPic, carX, carY,carAng) ;
+    }
+ }
+  // this.draw= function() {
   //   drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
   //   // // if(carPicLoaded){
   //   //     drawBitmapCenteredWithRotation(carPic, carX, carY,carAng) ;
   //   // }
   // }
-  this.draw= function() {
-    drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);
-    // // if(carPicLoaded){
-    //     drawBitmapCenteredWithRotation(carPic, carX, carY,carAng) ;
-    // }
-  }
-}
+
